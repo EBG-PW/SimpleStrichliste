260257877fd085c8e4e8825cd7c3b27b6b19deeb
BolverBlitz <bolverblitz@ebg.pw>
2025-11-02 19:38:44 +0100

    Some quality of life stuff

--- api/transactions.js
+++ api/transactions.js
@@ -1,6 +1,6 @@
 const { verifyRequest } = require('@middleware/verifyRequest');
 const { limiter } = require('@middleware/limiter');
-const { getTransactionHistory } = require('@lib/sqlite/users');
+const { getUserTransactionHistory, getAllTransactionHistory } = require('@lib/sqlite/users');
 const Joi = require('@lib/sanitizer');
 const HyperExpress = require('hyper-express');
 const router = new HyperExpress.Router();
@@ -12,12 +12,19 @@ const PluginVersion = '0.0.1'; //This plugins version
 
 const paginationSchema = Joi.object({
     limit: Joi.number().integer().min(1).max(100).default(10),
-    page: Joi.number().integer().min(1).default(1)
+    page: Joi.number().integer().min(1).default(1),
+    groupbyday: Joi.boolean().default(false),
 });
 
 router.get('/', verifyRequest('web.user.transactions.read'), limiter(4), async (req, res) => {
     const query = await paginationSchema.validateAsync(req.query);
-    const transactions = await getTransactionHistory(req.user.user_data.id, query.limit, query.page);
+    const transactions = await getUserTransactionHistory(req.user.user_data.id, query.limit, query.page, query.groupbyday);
+    res.json({ transactions });
+});
+
+router.get('/all', verifyRequest('web.admin.transactions.read'), limiter(4), async (req, res) => {
+    const query = await paginationSchema.validateAsync(req.query);
+    const transactions = await getAllTransactionHistory(query.limit, query.page, query.groupbyday);
     res.json({ transactions });
 });
 
